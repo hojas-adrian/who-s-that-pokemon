963b12016c54c3a853d473a3d6496c6fc3213cbe
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-07-26 14:45:41 -0400

    add error handler

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,9 @@
 import { Bot } from "./deps.ts";
 import { BOT_TOKEN } from "./helpers/constants.ts";
 import plugins from "./composer/plugins.ts";
+import onError from "./handlers/ch_error.ts";
 
 export const bot = new Bot(BOT_TOKEN);
 
 bot.use(plugins);
+bot.catch(onError);
