0cee92f9a47493c92019b71d915e8626b47f6c9c
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-07-29 13:29:19 -0400

    deploy

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,4 @@
-export const VERSION = [0, 0, 0];
+export const VERSION = [1, 0, 0];
 
 export const BOT_TOKEN = Deno.env.get("BOT_TOKEN") as string;
 export const IMGIX_URL = Deno.env.get("IMGIX_URL") as string;
